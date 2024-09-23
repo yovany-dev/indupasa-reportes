d0186a8f2cc6f7f54f1602d67fc1950d45e57aff
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2024-09-22 23:07:19 -0600

    feat: basic structure for pages

---
+++ src/pages/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <Title text="App Title" />
+      <v-col cols="12">
+        <p>Example</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  import Title from '@/common/components/Title.vue'
+</script>
+
+<style scoped lang="scss">
+</style>
