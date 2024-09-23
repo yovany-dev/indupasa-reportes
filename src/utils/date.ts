6ba43c49275f3dc23f4d900bd934f14161b070c7
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2024-09-22 23:02:32 -0600

    feat: add new utils

---
+++ src/utils/date.ts
@@ -0,0 +1,7 @@
+export function getDate(): string {
+  const today = new Date();
+  const day = today.getDate().toString().padStart(2, '0');
+  const month = (today.getMonth() + 1).toString().padStart(2, '0');
+  const year = today.getFullYear().toString();
+  return `${day}/${month}/${year}`;
+}
---
+++ src/utils/hour.ts
@@ -0,0 +1,6 @@
+export function getHour(): string {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  return `${hours}:${minutes}`;
+}
