91691e6d4e6ab7d5c6ae0832bc3c869142c184bf
Yovany Morales <83714102+yovany-dev@users.noreply.github.com>
2024-10-13 10:53:12 -0600

    feat(dashboard): change background gradient

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,14 @@
 <script lang="ts" setup>
   //
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+  * {
+    font-family: "Montserrat", serif !important;
+  }
+  body {
+    font-family: "Montserrat", serif !important;
+  }
+</style>
